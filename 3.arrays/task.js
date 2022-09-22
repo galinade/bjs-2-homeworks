2fcb9625617ada8dc70cc5db1291916f55e5c68c
Galina Demidova <galinagriz77@gmail.com>
2022-09-22 20:47:46 +0300

    Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
+  
+  result = arr1.length === arr2.length  &&
+        arr1.every((value, index) => value === arr2[index]); 
 
-  // Ваш код
-
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter(value => value > 0).filter(value => value %3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
