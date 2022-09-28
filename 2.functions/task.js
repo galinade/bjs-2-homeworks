ce0b3e9dbe40a5d8baeb82b366fa68005b9f0f36
Galina Demidova <galinagriz77@gmail.com>
2022-09-28 08:01:41 +0300

    Функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,71 +4,52 @@ function getArrayParams(arr) {
   min = arr[0];
   max = arr[0];
   sum = 0;
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] > max)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) { 
     max = arr[i];
-  }
-  for (let i = 0; i < arr.length; i++){
-    if(arr[i] < min)
-    min=arr[i];
-  }
-  for(let i = 0; i < arr.length; i++){
-    sum +=arr[i];
-  }
-  avg=+((sum/arr.length).toFixed(2));
+    }
+    if(arr[i] < min) {
+    min = arr[i];
+    }
 
+    sum +=arr[i];
+  
+    avg=+((sum/arr.length).toFixed(2));
+  }
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
- function worker(arr) {
-  let sum;
-    
-   sum = arr.reduce((acc,item) => acc + item, 0); 
-   return sum;
-  }
-
-
-function makeWork(arrOfArr, worker) {
+function worker(arr) {
+  return  arr.reduce((acc,item) => acc + item, 0);  
+}
+ 
+function makeWork(arrOfArr, func) {
   let max;
-  max = worker(arrOfArr [0]);
-   
-  
-  for (let i = 0; i < arrOfArr.length; i++){
-    if (worker(arrOfArr[i]) > max)
-    max = worker(arrOfArr[i]);
+  max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+    max = func(arrOfArr[i]);
+    }
   }
-  
   return max;
 }
-
+makeWork(arrOfArr, worker);
 // Задание 3
 function worker2(arr) {
-  
+
   let min = arr[0];
   let max = arr[0];
-  let difference =  min -max;
-   
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] > max)
-    max = arr[i];
-  }
 
-  for (let i = 0; i < arr.length; i++){
-    if(arr[i] < min)
+  for (let i = 0; i < arr.length; i++) { 
+    if (arr[i] > max) {
+    max = arr[i];
+    }
+    if(arr[i] < min) {
     min=arr[i]; 
+    }
+  }
+  return Math.abs(max - min);
 }
-  
-return Math.abs(difference);
-}
-function makeWork(arrOfArr, worker2) { 
-  let max1;
-  max1 = worker2(arrOfArr [0]);
-   
-  
-  for (let i = 0; i < arrOfArr.length; i++){
-    if (worker2(arrOfArr[i]) > max1)
-    max1 = worker2(arrOfArr[i]);
-  } 
-  return max1;
-}
+makeWork(arrOfArr, worker2);
+ 
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,24 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
+ student1 = new Student();
+ student2 = new Student();
+
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
+}
+Student.prototype.addMark(mark) = function (marks) {
+
+  if(this.marks === undefined){ 
+    this.mark = marks.push(mark[0]);
+    } else {
+     this.mark =marks.push(... mark) 
+    }
 }
 
 // ваш код для остальных методов
