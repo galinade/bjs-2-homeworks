c597b0a44ffe9b88e15fbae8fbf6f7c75a636782
Galina Demidova <galinagriz77@gmail.com>
2022-10-08 22:19:23 +0300

    Основные понятия - 1 задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,28 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let x1 = 0;
+  let x2 = 0;
+  let d = b**2-4*a*c;
+
+  if (d < 0) {
+    return arr = [];
+  } else {
+  if (d == 0) {
+    x1 = -b/(2*a);
+    
+    return arr =[x1];
+  } else {
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d) )/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2*a);
+   
+    return arr = [x1,x2]; 
+    }
+  }
 }
+} 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
