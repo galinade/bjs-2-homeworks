84f1f1126dd8a358cc328ea71f20ebf7a4b121d4
Galina Demidova <galinagriz77@gmail.com>
2022-10-13 13:01:50 +0300

    Проблемы с Дз Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,53 +1,48 @@
-function parseCount(parseInt) {
-   //if(typeof Number(parseInt) !== 'number' ) {
-    if (Number(parseInt)== NaN) {
-   
-    throw new Error ("Невалидное значение");
-   }
-   return Number(parseInt); 
+function parseCount(string) {
+    const parsed = Number.parseInt(string);
+    if(isNaN(parsed)) {  
+     throw new Error ("Невалидное значение");
+    }
+    return parsed; 
 }
 
-function validateCount(parseInt) {
-   // this.parseInt = 
-
-       function parseCount() 
-       
-}
-
-
-
-
-
-
-
-
-
-
-
-
+function validateCount(string) { 
+    try{
+      return parseCount(string);
+    } catch (error){
+        //return console.error("Ошибка");
+        console.log(error);
+    
+    }
+} 
 
 class Triangle {
-      constructor (a,b,c) {
+    constructor (a,b,c) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.P = 0;
+        this.p = 0;
+        this.S = 0;
+        
     
-    if (a + b < c || a + c < b || b +c < a) {
+    if (this.a + this.b < this.c || this.a + this.c < this.b || this.b +this.c <this.a) {
         throw new Error ("Треугольник с такими сторонами не существует");    
     }
-      }
-    getPerimeter(a,b,c) {
-        this.P = a + b + c;
-       return P;
+    }  
+    getPerimeter() {
+        return this.P = this.a + this.b + this.c;
+    
     }
 
-    getArea(a,b,c) {
-        this.p = (a + b + c)/2;
+    getArea() {
+        this.p = (this.a + this.b + this.c)/2;
         this.S = Math.sqrt(this.p*(this.p - this.a)*(this.p - this.b)*(this.p - this.c));
-        return +S.toFixed(3);
+        return +this.S.toFixed(3);
     }
-}
-    function getTriangle(a,b,c) {
-       return new Triangle;
 
-    }
+    //function getTriangle(a,b,c) {
+    //   return new Triangle;
+}
+
+
